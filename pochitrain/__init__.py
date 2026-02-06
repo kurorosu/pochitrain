b97a0958b1706b71f3326197fb8cb6ad9742835a
kurorosu <149222114+kurorosu@users.noreply.github.com>
2026-02-06 22:35:32 +0900

    chore: バージョンを1.4.0に更新

--- pochitrain/__init__.py
+++ pochitrain/__init__.py
@@ -28,7 +28,7 @@ from .pochi_trainer import PochiTrainer
 # ユーティリティ
 from .utils.directory_manager import InferenceWorkspaceManager, PochiWorkspaceManager
 
-__version__ = "1.3.1"
+__version__ = "1.4.0"
 __author__ = "Pochi Team"
 __email__ = "pochi@example.com"
 
